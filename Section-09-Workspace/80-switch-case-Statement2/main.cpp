1abfd6dc0b1df801f29f776b95ce2e7a9a4ea373
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-06-11 06:17:55 -0500

    Adding 80-switch-case-Statement2 files and directories

---
+++ Section-09-Workspace/80-switch-case-Statement2/main.cpp
@@ -0,0 +1,43 @@
+/*
+ *  Author: Matt Johnson
+ *  Subject: switch-case statements
+ *  Purpose: 
+ *  Date: 06/11/2023
+ * 
+ *  Requirements:
+ * 
+ *          Using enumeration        
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    
+    enum Direction {
+        left, right, up, down
+    };
+    
+    Direction heading {left};
+    
+    switch (heading) {
+        case left:
+            cout << "Going left" << endl;
+            break;
+        case right:
+            cout << "Going right" << endl;
+            break;
+        case up:
+            cout << "Going up" << endl;
+            break;
+        case down:
+            cout << "Going down" << endl;
+            break;
+        default:
+            cout << "OK" << endl;
+    }
+    
+    cout << endl;
+    return 0;
+}
