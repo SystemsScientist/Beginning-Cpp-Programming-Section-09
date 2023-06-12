822ee3cfbb5de7bc8da2988dbb85969600813452
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-06-12 06:35:23 -0500

    Adding 81-Conditional-Operator2 files and directories

---
+++ Section-09-Workspace/81-Conditional-Operator2/main.cpp
@@ -0,0 +1,35 @@
+/*
+ *  Author: Matt Johnson
+ *  Subject: conditional operator
+ *  Purpose: 
+ *  Date: 06/12/2023
+ * 
+ *  Requirements:
+ * 
+ *                  Declare variables
+ *                  Prompt user
+ *                  Process input using if-else block
+ *                  Process input using conditional operator
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    
+    int num1 {}, num2 {};
+    
+    cout << "Enter two integers separated by a space: ";
+    cin >> num1 >> num2;
+    
+    if (num1 != num2) {
+        cout << "Largest: " << ((num1 > num2) ? num1 : num2) << endl;
+        cout << "Smallest: " << ((num1 < num2)  ? num1 : num2) << endl;
+    } else {
+        cout << "The numbers are the same" << endl;
+    }
+    
+    cout << endl;
+    return 0;
+}
