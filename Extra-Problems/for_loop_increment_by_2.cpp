e28109fcf9c2fd3ae8285c6dc2741228feb9e06e
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-06-13 07:14:10 -0500

    Adding for_loop_increment_by_2.cpp file

---
+++ Extra-Problems/for_loop_increment_by_2.cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream> 
+
+using namespace std;
+
+int main(void) {
+
+	cout << "for loop increments by 2: " << endl;
+	for (int i{1}; i <= 10; i += 2)
+	       cout << i << endl;
+
+	cout << endl;
+	return 0;
+}	
