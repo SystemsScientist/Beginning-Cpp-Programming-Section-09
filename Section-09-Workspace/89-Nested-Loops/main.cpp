df0b96eb563bb1eaad00df5dd57dca545bc6827b
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-06-17 06:24:33 -0500

    Adding 89-Nested-Loops files and directories

---
+++ Section-09-Workspace/89-Nested-Loops/main.cpp
@@ -0,0 +1,27 @@
+/*
+ *  Author: Matt Johnson
+ *  Subject: nested loops
+ *  Purpose: program prints a multiplication table
+ *  Date: 06/17/2023
+ * 
+ *  Requirements:
+ * 
+ *                 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    
+    for (int num1 {1}; num1 <= 10; ++num1) {
+        for (int num2 {1}; num2 <= 10; ++num2) {
+            cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+        }
+        cout << "---------------" << endl;
+    }
+    
+    cout << endl;
+    return 0;
+}
