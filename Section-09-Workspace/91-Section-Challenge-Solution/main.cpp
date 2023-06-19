ccaf4130358c4a59b974041a41417b54a7fd3c24
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-06-19 06:28:10 -0500

    Updating 91-Section-Challenge-Solution files and directories

--- Section-09-Workspace/91-Section-Challenge-Solution/main.cpp
+++ Section-09-Workspace/91-Section-Challenge-Solution/main.cpp
@@ -1,7 +1,7 @@
 /*
  *  Author: Matt Johnson
  *  Subject: Section Challenge Solution
- *  Purpose: 
+ *  Purpose: program prints interactive interface
  *  Date: 06/18/2023
  */
 
@@ -9,6 +9,7 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <iomanip>
 
 using namespace std;
 
@@ -16,6 +17,8 @@ int main(void) {
     
     char choice {};
     vector <int> data;
+    double running_total {};
+    double mean {};
     
     do {
         cout << "\nP - Print numbers" << endl;
@@ -29,26 +32,54 @@ int main(void) {
         cin >> choice;
         
         if (choice == 'P' || choice == 'p') {
-            cout << "List of values " << "[ ";
-            for (long unsigned int i = 0; i < data.size(); i++)
-                cout << data[i] << " ";
-            cout << " ]";
-            cout << endl;
+            
+            if (data.size() == 0) {
+                cout << "[] - the list is empty" << endl;
+                cout << "Please add a number" << endl;
+            } else {            
+                cout << "List of values " << "[ ";
+                for (long unsigned int i = 0; i < data.size(); i++)
+                    cout << data[i] << " ";
+                cout << " ]";
+                cout << endl;
+            }
         } else if (choice == 'A' || choice == 'a') {
+            
             int data_item {};
             cout << "Add a number greater than zero: ";
             cin >> data_item;
             data.push_back(data_item);
+            cout << data_item << " added" << endl;
+            
+            running_total += data_item;
+            cout << "Running total " << running_total << endl;
         } else if (choice == 'M' || choice == 'm') {
-            cout << "Mean of the inputted values is " << endl;
+            
+            if (running_total != 0) {
+                mean = running_total / static_cast<double>(data.size());
+                cout << "Mean of list is " << setprecision(4) << mean << endl;
+            } else {
+                cout << "Unable to calculate the mean - no data" << endl;
+                cout << "Please add a number" << endl;
+            }
         } else if (choice == 'S' || choice == 's') {
-            cout << "Smallest number is " << *min_element(data.begin(), data.end()) << endl;
+            
+            if (data.size() == 0)
+                cout << "Please add a number" << endl;
+            else            
+                cout << "Smallest number is " << *min_element(data.begin(), data.end()) << endl;
         } else if (choice == 'L' || choice == 'l') {
-            cout << "Largest number is " <<  *max_element(data.begin(), data.end()) << endl;
+            
+            if (data.size() == 0)
+                cout << "Please add a number" << endl;
+            else
+                cout << "Largest number is " <<  *max_element(data.begin(), data.end()) << endl;
         } else if (choice == 'Q' || choice == 'q') {
+            
             cout << "Goodbye...\n" << endl;
             break;
         } else {
+            
             cout << "That is not an option." << endl;
             cout << "Please try again.\n" << endl;
         }
